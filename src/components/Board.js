761199211bcd1c7c91b867507a961b08adbf8303
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-05-20 12:32:02 +0300

    added first version

---
+++ src/components/Board.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import Square from './Square';
+
+const Board = ( { squares, onClick } ) => {
+
+    const renderSquare = (index) => {
+        return <Square value={squares[index]} onClick={ () => onClick(index) } />;
+    }
+
+    return (
+        <div>
+            <div className='board-row'>
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className='board-row'>
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className='board-row'>
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+};
+
+Board.propTypes = {
+    squares: PropTypes.arrayOf(PropTypes.oneOf('X', 'O', 'null')).isRequired,
+    onClick: PropTypes.func.isRequired,
+}
+
+export default Board;
